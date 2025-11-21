7996a6a7a0af4fdaea9213d6a17d4f7e9ed6bc5a
Tim <zhukai@boatlink-inc.com>
2025-11-21 17:30:19 +0800

    ViewBar修复

--- src/widgets/multi-view/ui/RefreshableOutlet.tsx
+++ src/widgets/multi-view/ui/RefreshableOutlet.tsx
@@ -1,8 +1,17 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
+import classes from './RefreshableOutlet.module.css'
 
 /**
  * 多视图 Outlet 组件
  */
 export function RefreshableOutlet() {
-  return <Outlet />
+  const location = useLocation()
+
+  return (
+    <div className={classes.wrapper}>
+      <div key={location.key} className={classes.content}>
+        <Outlet />
+      </div>
+    </div>
+  )
 }
